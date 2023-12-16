9b47195e26052b3a12c083dcaaa1b799a0aca9d1
oriri04 <152679239+oriri04@users.noreply.github.com>
2023-12-16 18:24:24 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+console.log (multiply); 
 
-// Write your code here
+let mod = 4;
+let num4 = 5;
+let num3  = (2 * num4) + mod
+console.log (num3); 
+
+let numberSet = [10, 5, 20, 15];
+let max = (20);
+console.log (max);
+
+let randomDecimal = Math.random();
+let random = Math.round(randomDecimal * 100) + 1;
+console.log (random);
